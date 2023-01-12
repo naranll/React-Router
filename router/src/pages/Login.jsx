4b455b4105d7c1793cc03350a2891b2d43d0d5a5
Morning <morning@admins-iMac.local>
2023-01-12 10:12:54 +0800

    another

---
+++ router/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Login() {
+    return <div>
+        Login
+        <Header />
+        <Footer />
+    </div>
+}

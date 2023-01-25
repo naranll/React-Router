13e7b279d64b3a8e46189d458aa53888fb3af1b0
Morning <morning@admins-iMac-7.local>
2023-01-25 12:05:19 +0800

    added server data  through axios

--- router/src/pages/Home.jsx
+++ router/src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import "../styles/home.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+    const [products, setProducts] = useState('');
+
+    useEffect(() => {
+        axios.get("http://localhost:2020/products")
+            .then(response => {
+                setProducts(response.data);
+            })
+            .catch(() => console.log("didn't axios"));
+    }, [])
+    //dependecy blank == will only work once
+
+    console.log("products in state", products);
+
     return <div className="home">
-        Home
+        {[...products].map((product, i) => {
+            return <div key={i}>
+                <h1>{product.name}</h1>
+            </div>
+        })}
     </div>
 }
